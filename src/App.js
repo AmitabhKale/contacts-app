1f0aba3e5557dcd9cb0ec5064cf9f609dddebfbb
Amitabh Kale <amitabhkale@gmail.com>
2023-03-05 12:43:05 +0530

    Redux Store Setup and Contact Reducer

--- src/App.js
+++ src/App.js
@@ -8,25 +8,33 @@ import About from "./pages/About";
 import NotFound from "./pages/NotFound";
 import Test from "./pages/Test";
 import EditContact from "./pages/EditContact";
+import { Provider } from "react-redux";
+import store from "./store";
 
 class App extends Component {
   render() {
     return (
-      <BrowserRouter>
-        <div>
-          <Header branding="Contacts App" />
-          <div className="container">
-            <Switch>
-              <Route exact path="/" component={Contacts} />
-              <Route exact path="/contacts/add" component={AddContact} />
-              <Route exact path="/contacts/edit/:id" component={EditContact} />
-              <Route exact path="/about" component={About} />
-              <Route exact path="/test" component={Test} />
-              <Route component={NotFound} />
-            </Switch>
+      <Provider store={store}>
+        <BrowserRouter>
+          <div>
+            <Header branding="Contacts App" />
+            <div className="container">
+              <Switch>
+                <Route exact path="/" component={Contacts} />
+                <Route exact path="/contacts/add" component={AddContact} />
+                <Route
+                  exact
+                  path="/contacts/edit/:id"
+                  component={EditContact}
+                />
+                <Route exact path="/about" component={About} />
+                <Route exact path="/test" component={Test} />
+                <Route component={NotFound} />
+              </Switch>
+            </div>
           </div>
-        </div>
-      </BrowserRouter>
+        </BrowserRouter>
+      </Provider>
     );
   }
 }
